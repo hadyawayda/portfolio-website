b205180820480b90c72c07e8bef85aad38f4e528
Hady <hady_awayda@hotmail.com>
2023-07-25 00:29:35 +0300

    Fixed bugs

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -17,24 +17,24 @@ function Navbar() {
 
   return (
     <div className="flex justify-between items-center w-full h-32 lg:py-10 lg:px-20">
-      <Link
-        href="/"
-        className="px-5 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700 animate">
-        Hady Awayda
+      <Link href="/" className="animate">
+        <button className="px-5 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700">
+          Hady Awayda
+        </button>
       </Link>
       <div className="flex justify-between items-center lg:w-96">
-        <Link href="#projects">
-          <button className="px-5 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700 animate early">
+        <Link href="#projects" className=" animate early">
+          <button className="px-5 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700">
             Projects
           </button>
         </Link>
-        <Link href="#projects">
-          <button className="px-5 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700 animate mid">
+        <Link href="#projects" className="animate mid">
+          <button className="px-5 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700">
             About Me
           </button>
         </Link>
-        <Link href="#projects">
-          <button className="px-5 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700 animate late">
+        <Link href="#projects" className="animate late">
+          <button className="px-5 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700">
             Contact Me
           </button>
         </Link>
