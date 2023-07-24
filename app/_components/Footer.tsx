29659bd21cf4dd97fcc65130f03bce9afd67c159
Hady <hady_awayda@hotmail.com>
2023-07-24 22:17:14 +0300

    Updated

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div className="flex flex-col justify-between">
+      <div>Upper</div>
+      <div>Lower</div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ app/_components/Navbar.tsx
@@ -0,0 +1,14 @@
+function Navbar() {
+  return (
+    <div className="flex justify-between">
+      <div>Logo</div>
+      <div className="flex justify-between">
+        <div>Projects</div>
+        <div>About Me</div>
+        <div>Contact Me</div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
