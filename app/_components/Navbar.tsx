09b891126aab347a140757882be6c4dbfa495a05
Hady <hady_awayda@hotmail.com>
2023-07-25 01:13:03 +0300

    Updated

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -18,23 +18,23 @@ function Navbar() {
   return (
     <div className="flex justify-between items-center w-full h-32 py-10 pr-2">
       <Link href="/" className="animate">
-        <button className="px-5 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700">
+        <button className="px-5 py-2 hover:bg-black hover:text-white font-semibold text-base rounded-full transition-all ease-in-out duration-700">
           Hady Awayda
         </button>
       </Link>
       <div className="flex justify-between items-center lg:w-96">
         <Link href="#projects" className=" animate early">
-          <button className="px-4 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700">
+          <button className="px-4 py-2 hover:bg-black hover:text-white font-semibold text-base rounded-full transition-all ease-in-out duration-700">
             Projects
           </button>
         </Link>
         <Link href="#projects" className="animate mid">
-          <button className="px-4 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700">
+          <button className="px-4 py-2 hover:bg-black hover:text-white font-semibold text-base rounded-full transition-all ease-in-out duration-700">
             About me
           </button>
         </Link>
         <Link href="#projects" className="animate late">
-          <button className="px-4 py-2 hover:bg-black hover:text-white font-semibold text-sm rounded-full transition-all ease-in-out duration-700">
+          <button className="px-4 py-2 hover:bg-black hover:text-white font-semibold text-base rounded-full transition-all ease-in-out duration-700">
             Contact me
           </button>
         </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 export default function Home() {
   return (
     <div className="flex justify-start items-center w-full h-full text-lg px-4">
-      <div className="flex flex-col justify-between h-72 w-1/2">
-        <h1 className="animate vlate text-6xl">Software & Web Developer</h1>
-        <h2 className="animate vvlate text-2xl">
+      <div className="flex flex-col justify-between h-72 w-1/2 font-variation">
+        <h1 className="animate vlate text-7xl w-full font-medium mb-8">
+          Software & Web Developer
+        </h1>
+        <h2 className="animate vvlate text-2xl mb-6">
           Hi, I'm Hady, an Award winning Developer based in Beirut, Lebanon.
         </h2>
-        <p className="animate vvvlate text-2xl">
+        <p className="animate vvvlate text-2xl mb-4">
           I love what I do, and I am very passionate about learning new
           technologies.
         </p>
