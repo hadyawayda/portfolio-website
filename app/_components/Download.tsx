02490f795f5d757f56eae31cf3afb00446691dfa
Hady <hady_awayda@hotmail.com>
2024-06-08 01:46:41 +0300

    Updated Resumé

--- app/_components/Download.tsx
+++ app/_components/Download.tsx
@@ -7,7 +7,7 @@ function Download() {
       href="/Hady_Awayda-Resumé.pdf"
       rel="noopener noreferrer"
       target="_blank"
-      download="Hady Awayda Resumé.pdf"
+      download="Hady_Awayda-Resumé.pdf"
       className="md:mt-8 xlate">
       <button className="flex justify-between gap-1 px-7 py-4 sm:px-6 sm:py-3 lg:py-3.5 xl:py-4 lg:px-7 xl:px-10 items-center rounded-xl hover:bg-red-600 text-xs sm:text-sm bg-neutral-950  tracking-wider text-white transition-all duration-500 ease-in-out expanded">
         Download Resumé
